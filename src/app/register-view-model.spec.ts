ba5cc521c93427d799d4ea9abbd37aa3c78cb5b3
DESKTOP-FD066BN\Jeoffy <jeoffy_hipolito@yahoo.com>
2021-03-11 16:20:33 +1300

    Initial commit for the angular project

---
+++ src/app/register-view-model.spec.ts
@@ -0,0 +1,7 @@
+import { RegisterViewModel } from './register-view-model';
+
+describe('RegisterViewModel', () => {
+  it('should create an instance', () => {
+    expect(new RegisterViewModel()).toBeTruthy();
+  });
+});
